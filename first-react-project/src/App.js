b3f53555aa699e2fa089f703c66d565830c9aa3f
raghavGuptaTTN <raghav.gupta@tothenew.com>
2020-04-30 15:53:19 +0530

    old learning comments removed for level up

--- first-react-project/src/App.js
+++ first-react-project/src/App.js
@@ -13,7 +13,6 @@ class App extends Component {
     otherProp : 'another prop is untouched'
   }
 
-  // added parameter for changing state. 
   switchNameHandler = (newName)=>{
     console.log("was clicked.");
 
@@ -41,9 +40,6 @@ class App extends Component {
   render() {
 
     const style = {
-      // we need to use camel case or either quotes for prop names.  
-      // always use quotes for css values. 
-      // this will be scoped to this component only. 
       backgroundColor : "cyan",
       font : 'inherit',
       border : '1px solid blue',
@@ -54,15 +50,8 @@ class App extends Component {
     return ( 
       <div className="App"> 
         <h1>first react app</h1>
-      
-      {/* here we are returning an arrow function which will not be executed 
-      here. 
-      when the click event will occur, it will be called and then
-      it will call the handler method and return its output.
-      
-      this is an inefficient way of doing this. so, may not be recommended.*/}
+
         <button 
-        // this is the JSX keyword to provide inline styling. 
           style = {style} 
         onClick={()=>this.switchNameHandler('manisha')}>Switch name</button>
 
@@ -76,21 +65,7 @@ class App extends Component {
         <Person 
           name = {this.state.persons[1].name} 
           age = {this.state.persons[1].age}
-          /**
-           * here we see that we can pass method as a reference in the prop obejct
-           * and this is very important to know. 
-           * here, the person component may want to change the state of it
-           * and it needs the method for that but doesnt has access to.
-           * so, we will pass it as a reference.
-           * the name click is up to us.  */ 
-          // click = {this.switchNameHandler}
           
-          /**
-           * to pass the parameters also with the method, we have 2 methods.
-           * one is above. 
-           * but the below method is more preferred. 
-           * we bind the methd with this and pass the parameter also. 
-           */
           click = {this.switchNameHandler.bind(this, 'gunjan')}
         
           > I am raghav's brother.</Person>
--- first-react-project/src/Person/Person.js
+++ first-react-project/src/Person/Person.js
@@ -1,17 +1,4 @@
 import React from 'react';
-// we need to write the extension of the file also, we can skip for js only
-/**
- * however it may seem ugly to import a css file in a js file. 
- * but we are actually not importing that, its just like telling the 
- * webpack bundler about it, it will automatically takke care of it and 
- * add it to the html. 
- * 
- * also, webpack automatically adds some prefixes for better rendering on
- * most other browsers as well. 
- * 
- * it includes this css as style tag in the html code. it creates a style tag 
- * for every css file. 
- */
 import './Person.css'
 
 
@@ -22,10 +9,6 @@ const person = (props)=>{
         <div className="Person">
             <p onClick = {props.click}>I am {props.name}. i am {props.age}.</p> 
             <p>{props.children}</p>
-            {/* see, here we require the closing tag for input because its JSX.
-            also, 
-            if we specify the value attribute here and dont give the onChange
-            handler, it gives error because then there is no use of input box. */}
             <input type = "text" value = {props.name} onChange = {props.changed}/>
         </div>
     )
