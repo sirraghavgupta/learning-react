c03b753cd0ed37dc98a2021c2aac02f0cc14ceac
raghavGuptaTTN <raghav.gupta@tothenew.com>
2020-04-30 16:40:15 +0530

    one method of implementinc conditionals implemented.

--- first-react-project/src/App.js
+++ first-react-project/src/App.js
@@ -10,7 +10,8 @@ class App extends Component {
       {name : "mohit", age:24},
       {name : "didu", age:26}
     ],
-    otherProp : 'another prop is untouched'
+    otherProp : 'another prop is untouched',
+    showPersons : false
   }
 
   switchNameHandler = (newName)=>{
@@ -37,6 +38,16 @@ class App extends Component {
     });
   }
 
+  togglePersonsHandler = ()=>{
+    console.log("toggled");
+    const doesShow = this.state.showPersons;
+    this.setState(
+      {
+        showPersons : !doesShow
+      }
+    );
+  }
+
   render() {
 
     const style = {
@@ -53,26 +64,45 @@ class App extends Component {
 
         <button 
           style = {style} 
-        onClick={()=>this.switchNameHandler('manisha')}>Switch name</button>
+        onClick={this.togglePersonsHandler}>Switch name</button>
 
         <p>I am another rendering</p>
 
-        <Person 
-          name = {this.state.persons[0].name} 
-          age = {this.state.persons[0].age} 
-          changed = {this.nameChangeHandler}/>
-
-        <Person 
-          name = {this.state.persons[1].name} 
-          age = {this.state.persons[1].age}
-          
-          click = {this.switchNameHandler.bind(this, 'gunjan')}
-        
-          > I am raghav's brother.</Person>
-
-        <Person 
-          name = {this.state.persons[2].name} 
-          age = {this.state.persons[2].age}/>
+        {
+          /**
+           * as we know that its all javascript although it looks html. 
+           * so, we enclose all this into {} and then we can use the 
+           * js statements here for manipulation and checking condns. 
+           * 
+           * however we cant use if else here. only use ternary operator. 
+           * block statements cany be used. bcoz its JSX, not JS
+           * 
+           * also, we enclose here all the content in the div so that 
+           * we can altogether show or remove it.
+           * 
+           * this method becomes very messy when our code becomes more nested. 
+           */
+          this.state.showPersons === true ? 
+          <div>
+            <Person 
+              name = {this.state.persons[0].name} 
+              age = {this.state.persons[0].age} 
+              changed = {this.nameChangeHandler}/>
+  
+            <Person 
+              name = {this.state.persons[1].name} 
+              age = {this.state.persons[1].age}
+              
+              click = {this.switchNameHandler.bind(this, 'gunjan')}
+            
+              > I am raghav's brother.</Person>
+  
+            <Person 
+              name = {this.state.persons[2].name} 
+              age = {this.state.persons[2].age}/>
+  
+          </div> : null
+        }
 
       </div>
     );
