15779c4ecbc930b8c22c0763fbc6a91c5466bae5
raghavGuptaTTN <raghav.gupta@tothenew.com>
2020-05-01 10:11:17 +0530

    learned how to output lists of components in react

--- first-react-project/src/App.js
+++ first-react-project/src/App.js
@@ -64,22 +64,13 @@ class App extends Component {
     if(this.state.showPersons){
       persons = (
         <div>
-            <Person 
-                name = {this.state.persons[0].name} 
-                age = {this.state.persons[0].age} 
-                changed = {this.nameChangeHandler}/>
-    
+           {
+             this.state.persons.map( person => 
               <Person 
-                name = {this.state.persons[1].name} 
-                age = {this.state.persons[1].age}
-                
-                click = {this.switchNameHandler.bind(this, 'gunjan')}
-              
-                > I am raghav's brother.</Person>
-    
-              <Person 
-                name = {this.state.persons[2].name} 
-                age = {this.state.persons[2].age}/>
+                name = {person.name} 
+                age = {person.age}   
+              />)
+           }
         </div>
       );
     }
@@ -90,7 +81,7 @@ class App extends Component {
 
         <button 
           style = {style} 
-        onClick={this.togglePersonsHandler}>Switch name</button>
+        onClick={this.togglePersonsHandler}>Toggle Components</button>
 
         <p>I am another rendering</p>
 
