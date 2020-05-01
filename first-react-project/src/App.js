f70219aff80470a6a7afa0e33d4cc447cbd07e4a
raghavGuptaTTN <raghav.gupta@tothenew.com>
2020-05-01 17:31:06 +0530

    styling components learned - normal styling, media queries and dynamic styling also - awesome.

--- first-react-project/src/App.js
+++ first-react-project/src/App.js
@@ -1,6 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+    background-color : ${ props => props.decideProp ? 'green' : 'red' };
+    font : inherit;
+    border : 1px solid blue;
+    padding : 8px;
+    cursor : pointer;
+
+    &:hover {
+      background-color : ${ props => props.decideProp ? 'lightgreen' : 'salmon' };
+      color : white;
+    }
+`;
+
+/**
+ * if we want to dynamically style these components, we can.
+ * we need to add a property to the component on the basis of which we want to 
+ * decide the styling. 
+ * so, we added the showPersons property. 
+ * 
+ * now, we use the above syntx with $ sign and we can use the JS code there. 
+ * after all, we know that its all JS. 
+ * `` makes a string templete which is parsed into JS after all by the 
+ * styled-components library. its his task. 
+ * 
+ * 
+ * now, we see that with over we give the 
+ * & sign - which tells that it has to apply the hover effect to this particular 
+ * component. 
+ * 
+ * also, we observe that in the html, finally different classes are made 
+ * acording to the property. and as the property value changes, we see 
+ * the classes swapped up in the chrome dev tools. 
+ */
 
 class App extends Component {
 
@@ -47,19 +82,7 @@ class App extends Component {
     );
   }
 
-
   render() {
-
-    const style = {
-      backgroundColor : "green",
-      font : 'inherit',
-      border : '1px solid blue',
-      padding : '8px',
-      cursor : 'pointer',
-      ':hover' : {
-        backgroundColor : 'lightgreen'
-      }
-    }
 
     let persons = null;
 
@@ -78,11 +101,6 @@ class App extends Component {
            }
         </div>
       );
-
-      style.backgroundColor = "red";
-      style[':hover'] = {
-        backgroundColor : 'salmon'
-      }
     }
 
     const classes = [];
@@ -100,9 +118,10 @@ class App extends Component {
           first react app
         </p>
 
-        <button 
-          style = {style} 
-        onClick={this.togglePersonsHandler}>Toggle Components</button>
+        <StyledButton 
+          onClick={this.togglePersonsHandler}
+          decideProp = {this.state.showPersons} > Toggle Components
+        </StyledButton>
 
         <p>I am another rendering</p>
 
